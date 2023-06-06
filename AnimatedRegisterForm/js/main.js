8f0df41c694590dfa63a7cbe0396415dc0fad45c
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-06-06 10:09:09 +0200

    Contact form - Completed

--- AnimatedRegisterForm/js/main.js
+++ AnimatedRegisterForm/js/main.js
@@ -1,2 +1,16 @@
-let thi = null;
-
+// document.addEventListener('DOMContentLoaded', () => {
+//     const form = document.getElementById('registerForm');
+//     form.addEventListener('submit', (event) => {
+//         event.preventDefault();
+//         const inputs = form.querySelectorAll('input');
+//         const values = Array.from(inputs).map((input) => input.value);
+//         console.log(values);
+        
+//         Swal.fire({
+//             title: 'Congrats',
+//             text: 'Data Successfully Logged',
+//             icon: 'success',
+//             confirmButtonText: 'OK'
+//         });
+//     });
+// });
